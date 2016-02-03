32ffe3763301263bcf5675481602510a1295d645
Miquel Adell Borràs <miquel.adell@gmail.com>
2016-02-03 13:59:10 +0100

    first parallax

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -74,4 +74,38 @@
   // Load Events
   $(document).ready(UTIL.loadEvents);
 
+
+
+
+  $.getScript('/app/themes/cordills/dist/scripts/animation.gsap.js', function() {
+    $.getScript('/app/themes/cordills/dist/scripts/debug.addIndicators.js', function() {
+
+      //// SCROLL MAGIC
+      var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+      // build scenes
+      new ScrollMagic.Scene({triggerElement: "#parallax1"})
+      .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+      .addTo(controller);
+
+      new ScrollMagic.Scene({triggerElement: "#parallax2"})
+      .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+      .addTo(controller);
+
+      new ScrollMagic.Scene({triggerElement: "#parallax3"})
+      .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+      .addTo(controller);
+
+      new ScrollMagic.Scene({triggerElement: "#parallax4"})
+      .setTween("#parallax4 > div", {y: "80%", ease: Linear.easeNone})
+      .addTo(controller);
+
+      new ScrollMagic.Scene({triggerElement: "#parallax5"})
+      .setTween("#parallax5 > div", {y: "80%", ease: Linear.easeNone})
+      .addTo(controller);
+    });
+  });
+
+
+
 })(jQuery); // Fully reference jQuery after this point.
--- front-page.php
+++ front-page.php
@@ -1,21 +1,39 @@
 <?php
 $args = array(
   'post_type' => 'page',
-  'post__in' => array( 7, 9, 5, 12 ), //list of page_ids
+  'post__in' => array( 7, 9, 5, 12, 16 ), //list of page_ids
   'order' => "ASC"
 );
 $page_query = new WP_Query( $args );
 ?>
 
+
+
+
 <?php if( $page_query->have_posts() ) : ?>
-	<ul class="front-page-sections">
+	<?php $i = 1; ?>
+
+	<div class="front-page-sections">
+
 		<?php while( $page_query->have_posts() ) : $page_query->the_post(); ?>
-			<li class="front-page-section" id="page_id-'<?=$post->ID?>'">
-				<?php get_template_part('templates/page', 'header'); ?>
-			    <?php get_template_part('templates/content', 'page'); ?>
-			</li>
+
+			<div class="front-page-section" id="page_id-<?=$post->ID?>">
+
+				<div id="parallax<?= $i; ?>" class="parallaxParent">
+					<div class="parallax-bacgkround" style="background-image: url('<?= get_template_directory_uri(); ?>/dist/images/section<?= $i; ?>.jpg');"></div>
+				</div>
+				<div class="spacer s1">
+					<div class="box2">
+						<?php get_template_part('templates/page', 'header'); ?>
+                        <div class="section-icon section-icon-<?= $i; ?>" style="background-image: url('<?= get_template_directory_uri(); ?>/dist/images/icon<?= $i; ?>.png');"></div>
+					    <?php get_template_part('templates/content', 'page'); ?>
+					</div>
+				</div>
+
+			</div>
+			<?php $i++; ?>
 		<?php endwhile; ?>
-	</ul>
+	</div> <!-- eo .front-page-sections -->
 <?php else:
 	throw new Exception("Missing posts", 1);
 endif; ?>
